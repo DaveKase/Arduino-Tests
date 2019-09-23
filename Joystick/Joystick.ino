17d5039c8b9c54466ed45634a003fab2ce6d4258
taaviKase <taavikase@gmail.com>
2019-09-23 11:19:15 +0300

    Added Arduino joystick test code

---
+++ Joystick/Joystick.ino
@@ -0,0 +1,33 @@
+#define VRx A3
+#define sw 2
+
+int xPos = 0;
+int swState = 0;
+int swCnt = 0;
+int rxLed = 17;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Joystick begin");
+  pinMode(rxLed, OUTPUT);
+  pinMode(VRx, INPUT);
+  pinMode(sw, INPUT_PULLUP);
+}
+
+void loop() {
+  xPos = analogRead(VRx);
+  swState = digitalRead(sw);
+  
+  if(swState == 0) {
+    swCnt++;
+    Serial.print("Switch count: ");
+    Serial.println(swCnt);
+
+    digitalWrite(rxLed, LOW);
+  }
+
+  Serial.print("x = ");
+  Serial.println(xPos);
+
+  digitalWrite(rxLed, HIGH);
+}
