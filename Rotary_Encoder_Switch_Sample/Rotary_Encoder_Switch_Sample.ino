a24a2fe2928ce27d417cc1ebad8806e7a6fa2885
taaviKase <taavikase@gmail.com>
2019-11-29 15:41:26 +0200

    Changed Rotary encoder code so that it will work better.

--- Rotary_Encoder_Switch_Sample/Rotary_Encoder_Switch_Sample.ino
+++ Rotary_Encoder_Switch_Sample/Rotary_Encoder_Switch_Sample.ino
@@ -1,55 +1,57 @@
 /*
- * This code is made to see how rotary encoder switch actually works
+ * This code is made to see how rotary encoder with switch actually works
  * Code idea from https://howtomechatronics.com/tutorials/arduino/rotary-encoder-works-use-arduino/
 */
 
 // Rotary Encoder connection pins
-#define clk 2
+#define clk 4
 #define dt 3
-#define sw 4
-
-// Onboard LED pin
-#define led 17
+#define sw 2
 
 // Variables used to read rotary encoder
 int lastState;
+int oldSwState = 0;
 int state;
 int pos = 0;
 int swState;
 int dtState;
 
 void setup() {
-  Serial.begin(9600); // Starting serial monitor
+  Serial.begin(9600);                         // Starting serial monitor
+  
   // Define rotary encoder pins as input (switch as input_pullup)
   pinMode(clk, INPUT);
   pinMode(dt, INPUT);
   pinMode(sw, INPUT_PULLUP);
 
-  // Define onboard LED pin as output (used with switch)
-  pinMode(led, OUTPUT);
-
-  // lastState variable used for encoder
-  lastState = digitalRead(clk);
+  lastState = digitalRead(clk);               // lastState variable used for encoder
 }
 
 void loop() {
-  // Getting clk pin state
-  state = digitalRead(clk);
-  
-  if(state != lastState) {      // If last clk state is not the same as current clk state
-    dtState = digitalRead(dt);  // Read dt state
-    
-    if(dtState != state) {      // If dtState is not the same as clk state (comes from the way rotary encoder works)
-      pos++;                    // Add 1 to pos
-    } else {                    // else
-      pos--;                    // Take away 1 from pos
-    }
-    
-    Serial.print("pos: ");      // Printing pos
-    Serial.println(pos);
+  swState = digitalRead(sw);                  // Reading the switch state
+
+  if(swState == LOW && oldSwState == 0) {     // If switch is pressed (LOW state) and old state was 0, oldSwState is set to 1, so we don't get multiple signals for one switch press
+    oldSwState = 1;
+    Serial.println("Switch press");           // Testing switch state output (should only be one for switch press)
   }
   
-  lastState = state;            // This is required to ensure that last state variable is updated with the new state
-  swState = digitalRead(sw);    // Reading switch state
-  digitalWrite(led, swState);   // Outputing switch's state to LED pin
+  if(swState == HIGH) {                       // If switch state is HIGH (not pressed), set oldSwState to 0 so we can get next switch press
+    oldSwState = 0;
+  }
+
+  state = digitalRead(clk);                   // Getting the clk pin state
+
+  if(state != lastState) {                    // If last clk state is not the same as current clk state
+    if(digitalRead(dt) != state) {            // If dt value is not the same as clk, the encoder was turned clockwise, add one to position variable
+      pos++;
+    } else {                                  // Else it was turned counter-clockwise
+      if(pos > 0) {                           // If pos is not less than 0, take one off from position variable
+        pos--;
+      }
+    }
+
+    lastState = state;                        // This is required to ensure that last state variable is updated with the new state
+    Serial.print("Position = ");              // Printing the position out
+    Serial.println(pos);
+  }
 }
