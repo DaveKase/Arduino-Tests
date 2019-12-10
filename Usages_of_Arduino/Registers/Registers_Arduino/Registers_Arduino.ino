53cf2b51eb812e823ac0df541f64b9df14f128d2
taaviKase <taavikase@gmail.com>
2019-12-10 10:49:41 +0200

    Variables now hold output data in registers testing

--- Usages_of_Arduino/Registers/Registers_Arduino/Registers_Arduino.ino
+++ Usages_of_Arduino/Registers/Registers_Arduino/Registers_Arduino.ino
@@ -3,12 +3,16 @@
  * Idea from: https://www.eprojectszone.com/how-to-control-arduino-pins-from-registers-without-digitalwrite-and-digitalread/
  */
 
+byte allOut = B11111111;
+byte noTxOut = B11110111;
+byte noTxRxOut = B11110011;
+
 void setup() {
-  DDRB = B11111111;       // Sets all ports as outputs, except RX/TX pins. Done through register, which gives faster pin allocation
-  DDRC = B11111111;
-  DDRD = B11110111;
-  DDRE = B11111111;
-  DDRF = B11111111;
+  DDRB = allOut;       // Sets all ports as outputs, except RX/TX pins. Done through register, which gives faster pin allocation
+  DDRC = allOut;
+  DDRD = noTxOut;
+  DDRE = allOut;
+  DDRF = allOut;
 }
 
 void loop() {
@@ -19,9 +23,9 @@ void loop() {
  * Sets all registers HIGH, except TX/RX pins
  */
 void setOutputsRegister() {
-  PORTB = B11111111;
-  PORTC = B11111111;
-  PORTD = B11110011;
-  PORTE = B11111111;
-  PORTF = B11111111;
+  PORTB = allOut;
+  PORTC = allOut;
+  PORTD = noTxRxOut;
+  PORTE = allOut;
+  PORTF = allOut;
 }
