c2b810a5eac4e18620abde71a7dd41ba9ac7059c
taaviKase <taavikase@gmail.com>
2019-12-17 16:18:06 +0200

    Added some more code and commets to Replace_delay sample

--- Usages_of_Arduino/Replace_Delay/Replace_Delay.ino
+++ Usages_of_Arduino/Replace_Delay/Replace_Delay.ino
@@ -20,8 +20,47 @@ int longInterval = 5000;
 
 void setup() {
   Serial.begin(9600);                                         // Start serial connection
+  while(!Serial);
   shortStartTime = millis();                                  // Initialize short start time variable
   longStartTime = millis();                                   // Initialize long start time variable
+  differentFor();                                             // Showing that for loop can be used with logic statements
+  twoDimensionalArrays();                                     // Shows how to deal with two dimensional arrays. Same logic can be used with more than two dimensional arrays.
+}
+
+/*
+ * Showing for loop with logic statement
+ * The inner for with j++ & 3 argument can be set up so
+ * that it runs infinitely. Basically it just adds one to j
+ * until j value is 3 and starts from zero again.
+ */
+void differentFor() {
+  for(int i = 0; i < 2; i++) {
+    for(int j = 0; j < 3; j++ & 3) {
+      Serial.print("J = ");
+      Serial.println(j);
+    }
+  }
+}
+
+/*
+ * Two dimensional arrays can hold different values for one variable.
+ * Used in many occasions in programming world.
+ * Same logic can be used with more than just two dimensional arrays.
+ */
+void twoDimensionalArrays() {
+  int dualArray[2][3] = {{50, 60, 70}, {32, 42, 66}};
+
+  for(int i = 0; i < 2; i++) {
+    Serial.print("A = ");
+    Serial.println(i);
+    
+    for(int j = 0; j < 3; j++) {
+      int b = dualArray[i][j];
+
+      Serial.print("B = ");
+      Serial.println(b);
+    }
+  }
 }
 
 void loop() {
