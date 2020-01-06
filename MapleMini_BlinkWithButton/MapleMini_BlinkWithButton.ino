48f72925858f61a928de02eba88c0895edc146d8
taaviKase <taavikase@gmail.com>
2020-01-06 11:23:27 +0200

    Some code changes and comments added to MapleMini_BlinkWithButton script

--- MapleMini_BlinkWithButton/MapleMini_BlinkWithButton.ino
+++ MapleMini_BlinkWithButton/MapleMini_BlinkWithButton.ino
@@ -1,61 +1,74 @@
-#define btn 32
+/*
+ * This little script has two blinking modes, fast and slow.
+ * It starts out with fast blinking mode.
+ * If the button (pin 32) is pressed, the mode changes.
+ * Blinking is done with reading milliseconds and calculating the intervals instead of using delay() method
+ * because when using delay() the reading of button presses is not reliable.
+ */
 
-bool isSlowBlink = false;
-unsigned long previousMillis = 0;
-const long slowInterval = 1000;
-const long fastInterval = 50;
-int ledState = LOW;
+#define btn 32                                            // Define pin 32 as btn
+
+const long fastInterval = 50;                             // Defining fast interval
+const long slowInterval = 1000;                           // Defining slow interval
+unsigned long previousMillis = 0;                         // Initialize previous millis
+
+bool isSlowBlink = false;                                 // Initialize fast blinking mode
+int ledState = LOW;                                       // Initialize LED state as LOW, meaning LED is turned off
 
 void setup() {
-  Serial.begin(115200); 
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(btn, INPUT_PULLUP);
+  Serial.begin(115200);                                   // Start serial communication
+  pinMode(LED_BUILTIN, OUTPUT);                           // Defien builtin LED pin as output
+  pinMode(btn, INPUT_PULLUP);                             // Define button as INPUT_PULLUP
 }
 
 void loop() {
-  unsigned long currentMillis = millis();
-  
-  int btnPress = digitalRead(btn);
-  Serial.print("Button = ");
-  Serial.println(btnPress);
+  unsigned long currentMillis = millis();                 // Get current time in millis
+  int btnPress = digitalRead(btn);                        // Read button presses
 
-  if(btnPress == 1) {
+  if(btnPress == 1) {                                     // If button was pressed reverse isSlowBlink value
     isSlowBlink = !isSlowBlink;
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
+    delay(500);
   }
 
-  if(isSlowBlink) {
+  if(isSlowBlink) {                                       // If isSlowBlink is true, call slowBlink() method, else call fast blink method
     slowBlink(currentMillis);
   } else {
     fastBlink(currentMillis);
   }
 }
 
+/*
+ * Checks if fast interval has passed
+ * @param currentMillis - current time in milliseconds
+ */
 void fastBlink(unsigned long currentMillis) {
-  if(currentMillis - previousMillis >= fastInterval) {
-    previousMillis = currentMillis;
-    
-    if(ledState == LOW) {
-      ledState = HIGH;
-    } else {
-      ledState = LOW;
-    }
-
-    digitalWrite(LED_BUILTIN, ledState);
+  if(currentMillis - previousMillis >= fastInterval) {    // If fast interval has passed
+    blinkLed(currentMillis);                              // Blink LED
   }
 }
 
+/*
+ * Checks if slow interval has passed
+ * @param currentMillis - current time in milliseconds
+ */
 void slowBlink(unsigned long currentMillis) {
-  if(currentMillis - previousMillis >= slowInterval) {
-    previousMillis = currentMillis;
+  if(currentMillis - previousMillis >= slowInterval) {    // If interval has passed
+    blinkLed(currentMillis);                              // Blink LED
+  }
+}
 
-    if(ledState == LOW) {
+/*
+ * Blinks the LED given current LED state
+ * @param currentMillis - current time in milliseconds
+ */
+void blinkLed(unsigned long currentMillis) {
+  previousMillis = currentMillis;                         // Save last blink time in millis
+  
+  if(ledState == LOW) {                                   // If the LED state is low (meaning off), turn it high (meaning on)
       ledState = HIGH;
     } else {
       ledState = LOW;
     }
 
-    digitalWrite(LED_BUILTIN, ledState);
-  }
+    digitalWrite(LED_BUILTIN, ledState);                  // Execute the order, meaning turn the LED to state required
 }
