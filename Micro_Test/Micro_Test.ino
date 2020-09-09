018846428e2da8de2cf0239b4a19b9674719e149
taaviKase <taavikase@gmail.com>
2020-09-09 09:35:03 +0300

    Redone the micro tester so that it will blink the RX LED and keep all pins as HIGH.

--- Micro_Test/Micro_Test.ino
+++ Micro_Test/Micro_Test.ino
@@ -5,28 +5,25 @@
 
 // Array contains all Arduino input/output pins, except TX/RX
 int pins[16] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 14, 15, 16, A0, A1, A2, A3};
+int led = 17;
 
 void setup() {
   // Set all pins in array to output
   for(int i = 0; i < 16; i++) {
     pinMode(pins[i], OUTPUT);
   }
-}
+  
+  pinMode(led, OUTPUT);
 
-void loop() {
   // Turn all pins in array to high
   for(int i = 0; i < 16; i++) {
     digitalWrite(pins[i], HIGH);
   }
+}
 
-  // Wait for 0.25 seconds
+void loop() {
+  digitalWrite(led, HIGH);
   delay(250);
-
-  // Turn all pins in array to low
-  for(int i = 0; i < 16; i++) {
-    digitalWrite(pins[i], LOW);
-  }
-
-  // Wait for 0.25 seconds again. Used to blink the LED. Using faster blink, so we can test the board faster.
+  digitalWrite(led, LOW);
   delay(250);
 }
